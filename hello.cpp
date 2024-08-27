1fe54ae1bae0831f31524e8d085a0b183778f708
Ralph S Landicho <rlandicho2@students.avc.edu>
2024-08-27 11:39:23 -0700

    More Comments

--- hello.cpp
+++ hello.cpp
@@ -30,13 +30,20 @@ int main() {
   To go back to a directory, type: cd ..
   To open terminal, type: CNTRL + J
   To make a new terminal, type: CNTRL + SHIFT + `
+  If this file committed is new, type: git add [file_name]
+  To commit, type: git commit . -m "add hello.cpp"
+  To push, type: git push
   */
+
+  //NOTE: I am aware that I don't need to format a Hello World application like this, but this is a refresher of OOP for me
 
   Hello myHello;
   string Hello = "Hello World";
 
   // call functions
   myHello.AssignHelloVariable(Hello);
+
+  //print function
   myHello.HelloFunction();
 
   return 0;
